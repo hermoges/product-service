fba7ce3cd92c92f8d381aa50a17d2c13aa71d627
hermoges <herm1mmu@gmail.com>
2026-04-25 22:27:35 +0300

    Fix error

--- product-service/src/main/java/com/ctbe/hermela/exception/ResourceNotFoundException.java
+++ product-service/src/main/java/com/ctbe/hermela/exception/ResourceNotFoundException.java
@@ -1,5 +1,7 @@
 package com.ctbe.hermela.exception;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     private final Long id;
 
